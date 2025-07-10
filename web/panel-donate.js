956b38599817c1ba6721a1a19330e3790b6a8193
etofok <etofok@gmail.com>
2025-07-10 17:03:40 +0300

    .

--- web/panel-donate.js
+++ web/panel-donate.js
@@ -1,5 +1,25 @@
-    function toggleDonatePanel(event) {
-      event.preventDefault();
-      const panel = document.getElementById('donateOverlay');
-      panel.style.display = panel.style.display === 'flex' ? 'none' : 'flex';
+// toggle panel on/off on click and set state
+function toggleDonatePanel(event) {
+    event.preventDefault();
+    const panel = document.getElementById('donateOverlay');
+
+    if (panel.style.display === 'flex') {
+        panel.style.display = 'none';
+        if (history.state && history.state.panelOpen) {
+            history.replaceState({}, document.title, window.location.pathname + window.location.search);
+        }
+    } else {
+        panel.style.display = 'flex';
+        history.pushState({ panelOpen: true }, 'Donate Panel Open', '#donate-panel-open');
     }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('popstate', function(event) {
+        const panel = document.getElementById('donateOverlay');
+
+        if (panel.style.display === 'flex' && (!event.state || !event.state.panelOpen)) {
+            panel.style.display = 'none';
+        }
+    });
+});
